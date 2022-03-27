f862b071fd26e4c65be846100c1a58403f168369
Shailseen <shailesh13246@gmail.com>
2022-03-27 12:38:08 +0530

    feat: set document title

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,8 +1,12 @@
 import { Carousel } from "../../components";
 import { Arrival } from "../../components/arrival/Arrival";
 import { Poster } from "../../components/poster/Poster";
+import { useEffect } from "react";
 
 export const Homepage = () => {
+  useEffect(()=>{
+    document.title = "Smasher | Home"
+  },[])
   return (
     <>
       <Carousel />
