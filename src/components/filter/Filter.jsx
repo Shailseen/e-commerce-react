a70e700e02d4bb8f6b51f3e0aba46cf8f0a6e3a0
Shailseen <shailesh13246@gmail.com>
2022-03-27 12:30:36 +0530

    feat: link label with input

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -10,33 +10,38 @@ export const Filter = ({ dispatch, state }) => {
         </a>
       </div>
       <div className="sorting-container border-bottom">
-      <h4 className="txt-align-left">Sort Price</h4>
-        <input className="place-left"
+        <h4 className="txt-align-left">Sort Price</h4>
+        <input
+          className="place-left"
           type="radio"
           name="sortData"
+          id="Low to high"
           checked={state.sortBy === "LOW_TO_HIGH"}
           onChange={() => dispatch({ type: "LOW_TO_HIGH" })}
         />
-        <label>Low To High</label>
+        <label for="Low to high">Low To High</label>
         <br />
-        <input className="place-left"
+        <input
+          className="place-left"
           type="radio"
+          id="High to low"
           name="sortData"
           checked={state.sortBy === "HIGH_TO_LOW"}
           onChange={() => dispatch({ type: "HIGH_TO_LOW" })}
         />
-        <label>High To Low</label>
+        <label for="High to low">High To Low</label>
       </div>
 
       <div className="cateogry-container border-bottom">
         <h4 className="txt-align-left">Categories</h4>
         <li className="cateogry-list-container list-style-none">
           <input
+            id="Include out of stock"
             type="checkbox"
             checked={includeOutOfStock}
             onChange={() => dispatch({ type: "INCLUDE_OUT_OF_STOCK" })}
           />
-          <label>Include out of stock</label>
+          <label for="Include out of stock">Include out of stock</label>
         </li>
       </div>
       <div className="category-container border-bottom">
@@ -44,58 +49,63 @@ export const Filter = ({ dispatch, state }) => {
 
         <li className="cateogry-list-container list-style-none">
           <input
+            id="Volley ball"
             type="checkbox"
             checked={state.volleyBall}
             onChange={() => dispatch({ type: "VOLLEY_BALL" })}
           />
-          <label>Volley Ball</label>
+          <label for="Volley ball">Volley Ball</label>
         </li>
 
         <li className="cateogry-list-container list-style-none">
           <input
+            id="Basket ball"
             type="checkbox"
             checked={state.basketBall}
             onChange={() => dispatch({ type: "BASKET_BALL" })}
           />
-          <label>Basket Ball</label>
+          <label for="Basket ball">Basket Ball</label>
         </li>
 
         <li className="cateogry-list-container list-style-none">
           <input
+            id="Tennis ball"
             type="checkbox"
             checked={state.tennisBall}
             onChange={() => dispatch({ type: "TENNIS_BALL" })}
           />
-          <label>Tennis Ball</label>
+          <label for="Tennis ball">Tennis Ball</label>
         </li>
 
         <li className="cateogry-list-container list-style-none">
           <input
+            id="Base ball"
             type="checkbox"
             checked={state.baseBall}
             onChange={() => dispatch({ type: "BASE_BALL" })}
           />
-          <label>Base Ball</label>
+          <label for="Base ball">Base Ball</label>
         </li>
 
         <li className="cateogry-list-container list-style-none">
           <input
+            id="Soccer ball"
             type="checkbox"
             checked={state.soccerBall}
             onChange={() => dispatch({ type: "SOCCER_BALL" })}
           />
-          <label>Soccer Ball</label>
+          <label for="Soccer ball">Soccer Ball</label>
         </li>
 
         <li className="cateogry-list-container list-style-none">
           <input
+            id="Golf ball"
             type="checkbox"
             checked={state.golfBall}
             onChange={() => dispatch({ type: "GOLF_BALL" })}
           />
-          <label>Golf Ball</label>
+          <label for="Golf ball">Golf Ball</label>
         </li>
-
       </div>
     </aside>
   );
