7667fcab7145f4f4077d5dd1659cc1df3383756d
Shailseen <shailesh13246@gmail.com>
2022-03-27 12:33:02 +0530

    style: move static data seprate file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,4 @@
-const footerList = [
-  {
-    id: 1,
-    icon: "fab fa-github",
-    profileLink: "https://github.com/Shailseen"
-  },
-  {
-    id: 2,
-    icon: "fab fa-twitter",
-    profileLink: "https://twitter.com/Shailes62273951"
-  },
-  {
-    id: 3,
-    icon: "fab fa-linkedin-in",
-    profileLink: "https://www.linkedin.com/in/shailesh-kumar-840b28220/"
-  }
-
-]
+import { footerList } from "./footerList";
 
 export const Footer = () => {
   return (
