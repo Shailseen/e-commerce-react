521e2e9fa341a728ff6f832ca53e91dd56f9ecdd
Shailseen <shailesh13246@gmail.com>
2022-03-31 23:11:07 +0530

    refactor: convert to jsx

--- src/components/cart-price-card/cart-price-card.jsx
+++ src/components/cart-price-card/cart-price-card.jsx
@@ -1,26 +1,26 @@
 import "./cart-price-card.css"
 export const CartPriceCard = () => {
     return (
-        <div class="price-detail-container box-shadow justify-center">
-                <label class="t-bold border-bottom p-b-1rm">Price details</label>
-                <div class="space-btw p-b-1rm">
+        <div className="price-detail-container box-shadow justify-center">
+                <label className="t-bold border-bottom p-b-1rm">Price details</label>
+                <div className="space-btw p-b-1rm">
                     <label>Price (2 items)</label>
                     <label>₹4999</label>
                 </div>
-                <div class="space-btw p-b-1rm">
+                <div className="space-btw p-b-1rm">
                     <label>Discount</label>
-                    <label class="green-color">-₹999</label>
+                    <label className="green-color">-₹999</label>
                 </div>
-                <div class="space-btw border-bottom p-b-1rm">
+                <div className="space-btw border-bottom p-b-1rm">
                     <label>Delivery Charges</label>
-                    <label class="green-color">FREE</label>
+                    <label className="green-color">FREE</label>
                 </div>
-                <div class="space-btw border-bottom p-b-1rm">
-                    <label class="t-bold">TOTAL AMOUNT</label>
-                    <label class="t-bold">₹4000</label>
+                <div className="space-btw border-bottom p-b-1rm">
+                    <label className="t-bold">TOTAL AMOUNT</label>
+                    <label className="t-bold">₹4000</label>
                 </div>
-                <p class="green-color">You will save ₹999 on this order</p>
-                <button class="button-style-none solid-button w-100">PLACE ORDER</button>
+                <p className="green-color">You will save ₹999 on this order</p>
+                <button className="button-style-none solid-button w-100">PLACE ORDER</button>
               </div>
     )
 }
--- src/components/product-cart-card/product-cart-card.jsx
+++ src/components/product-cart-card/product-cart-card.jsx
@@ -2,26 +2,26 @@ import "./product-cart-card.css"
 
 export const ProductCartCard = () =>{
     return (
-        <div class="card-items-container justify-center">
-                <div class="horizontal-card-output box-shadow">
-                    <div class="horizontal-card">
-                      <div class="card-img img-center">
-                        <img class="img-responsive with-border" src="/assets/81eRVhCsATL._SX425_.jpg" alt="volleyball"/>
+        <div className="card-items-container justify-center">
+                <div className="horizontal-card-output box-shadow">
+                    <div className="horizontal-card">
+                      <div className="card-img img-center">
+                        <img className="img-responsive with-border" src="/assets/81eRVhCsATL._SX425_.jpg" alt="volleyball"/>
                       </div>
-                      <div class="card-description-container">
-                      <div class="text-container text-grid">
+                      <div className="card-description-container">
+                      <div className="text-container text-grid">
                         <h4>Nivia Kross World Volleyball</h4>
-                        <p class="price-card"><span class="original-price">$100</span> <span class="price-discount">$149</span> <span class="price-percantage">33%</span></p>
-                        <div class="quantity-container">
+                        <p className="price-card"><span className="original-price">$100</span> <span className="price-discount">$149</span> <span className="price-percantage">33%</span></p>
+                        <div className="quantity-container">
                             <label>Quantity:</label>
-                            <button class="outline-round-button">-</button>
-                            <input type="text" class="quantity-input-box" value="1"/>
-                            <button class="outline-round-button">+</button>
+                            <button className="outline-round-button">-</button>
+                            <input type="text" className="quantity-input-box" value="1"/>
+                            <button className="outline-round-button">+</button>
                         </div>
                       </div>
-                      <div class="card-button-container">
-                        <button class="button-style-none secondary-button w-100 white-color">Remove From Cart</button>
-                        <button class="button-style-none outline-secondary-button w-100">Move to Wishlist</button>
+                      <div className="card-button-container">
+                        <button className="button-style-none secondary-button w-100 white-color">Remove From Cart</button>
+                        <button className="button-style-none outline-secondary-button w-100">Move to Wishlist</button>
                       </div>
                      </div>
                     </div>
